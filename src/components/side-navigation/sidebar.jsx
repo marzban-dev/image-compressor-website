b16525a5f3fef49ec24824256891aa6cd414f8af
Nima <nimiologyy@gmail.com>
2023-04-08 01:31:27 +0430

    background

--- src/components/side-navigation/sidebar.jsx
+++ src/components/side-navigation/sidebar.jsx
@@ -4,6 +4,12 @@ import './sidebar.css'
 function Sidebar(){
     return (
         <div className="sidebar">
+            <ul>
+                <li>Welcome</li>
+                <li>About</li>
+                <li>Features</li>
+                <li>Download</li>
+            </ul>
         </div>
     )
 }
